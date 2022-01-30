242f8fe7a5c786419bf83843335114e3d5e6942f
Grzegorz Araminowicz <g.araminowicz@gmail.com>
2022-01-30 18:45:43 +0100

    allow deleting boats and users

--- routes/boats.js
+++ routes/boats.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const toDto = require('../model/boat.js').toDto;
-const boats = require('../sim.js').boats;
+let boats = require('../sim.js').boats;
 
 router.get('/', (req, res) => {
     res.send(boats.map(boat => toDto(boat)));
@@ -12,6 +12,16 @@ router.get('/:id', (req, res) => {
     let found = boats.find(item => item.id == req.params.id);
     if (found) {
         res.send(toDto(found));
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    let found = boats.find(item => item.id == req.params.id);
+    if (found) {
+        boats = boats.filter(item => item.id != req.params.id);
+        res.end();
     } else {
         res.sendStatus(404);
     }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const users = require('../data/users.json');
+let users = require('../data/users.json');
 
 router.get('/', (req, res) => {
     res.send(users);
@@ -11,6 +11,16 @@ router.get('/:id', (req, res) => {
     let found = users.find(item => item.id == req.params.id);
     if (found) {
         res.send(found);
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    let found = users.find(item => item.id == req.params.id);
+    if (found) {
+        boats = users.filter(item => item.id != req.params.id);
+        res.end();
     } else {
         res.sendStatus(404);
     }
